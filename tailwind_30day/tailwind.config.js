2698f668700431f38a2ec36b3e3798ca3bd56104
liaojiayun <liaojiayun@bytedance.com>
2022-08-22 14:45:11 +0800

    feat: add some config

--- tailwind_30day/tailwind.config.js
+++ tailwind_30day/tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     content: ["./*.html", "./src/**/*.html", "./src/**/*.js"],
   },
   darkMode: false, // or 'media' or 'class'
-  theme: {},
+  theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1400px",
+    },
+    extend: {
+      screens: {
+        "3xl": "1600px",
+        "4xl": "2000px",
+      },
+    },
+  },
   variants: {
     extend: {},
   },
